8c30a71b35162e8affb93ee8b28524bbefe4d8e3
wolfybauer <wolfy.bauer2@gmail.com>
2023-02-02 08:14:13 -0800

    working. tile ysort still weird

--- world.gd
+++ world.gd
@@ -5,15 +5,29 @@ extends Node2D
 var tile_scale:Vector2i
 var MAX_HEIGHT = 4 # zero indexed, temporary. aka terrain map atlas coords max x
 
+# altitude seperators
+@export var LOWLAND_START := 0.045
+@export var MIDLAND_START := 0.2
+@export var HIGHLAND_START := 0.8
+@export var MOUNTAIN_START := 0.95
+
+# temp seperators
+@export var MID_TEMP := 0.2
+@export var HIGH_TEMP := 0.9
+
+# humidity
+@export var MID_HUMID := 0.3
+@export var HIGH_HUMID := 0.6
+
 @onready var tmap:TileMap = $TerrainMap
 @onready var rmap:TileMap = $ReplacerMap
 
-@onready var alt_sprite:Sprite2D = $AltitudeNoise
-@onready var temp_sprite:Sprite2D = $TemperatureNoise
-@onready var moist_sprite:Sprite2D = $MoistureNoise
-@onready var alt_noise:FastNoiseLite = $AltitudeNoise.texture.noise
-@onready var temp_noise:FastNoiseLite = $TemperatureNoise.texture.noise
-@onready var moist_noise:FastNoiseLite = $MoistureNoise.texture.noise
+@onready var alt_noise:FastNoiseLite = $NoiseMaps/AltitudeNoise.texture.noise
+@onready var temp_noise:FastNoiseLite = $NoiseMaps/TemperatureNoise.texture.noise
+@onready var moist_noise:FastNoiseLite = $NoiseMaps/MoistureNoise.texture.noise
+
+#@onready var player = $TerrainMap/Player
+@onready var player = $Player
 
 const tile_type = {
 	'water' : 0,
@@ -32,6 +46,29 @@ const HEIGHT_TO_ATLAS = {
 	4 : 9,
 }
 
+const biome_data = {
+	'plains' : { 'drygrass' : 1.0 },
+	'beach' : { 'sand' : 1.0, 'dirt' : 0.0 },
+	'jungle' : { 'wetgrass' : 1.0 },
+	'desert' : { 'sand' : 0.8, 'dirt' : 0.2 },
+	'highlands' : { 'dirt' : 0.95, 'water' : 0.05 },
+	'mountain' : { 'dirt' : 0.95, 'snow' : 0.05 },
+	'water' : { 'water' : 1.0 }
+}
+
+const object_data = {
+	'plains' : { 'palm' : 0.005 },
+	'beach' : { 'rock' : 0.0005 },
+	'jungle' : { 'tree' : 0.01 },
+	'desert' : { 'rock' : 0.003, 'cacti' : 0.01, 'palm' : 0.0005 },
+	'highlands' : { 'tree' : 0.01 },
+	'mountain' : { 'rock' : 0.001},
+	'water' : {}
+}
+
+# utility
+func in_range(val, lo, hi):
+	return val >= lo and val < hi
 func how_high(alt):
 	var inc = 1.0 / float(MAX_HEIGHT + 1)
 	for h in range(1,MAX_HEIGHT+1):
@@ -44,9 +81,7 @@ func how_high(alt):
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# make the noise sprites invisible
-	alt_sprite.visible = false
-	temp_sprite.visible = false
-	moist_sprite.visible = false
+	$NoiseMaps.visible = false
 	
 	# set noise seeds if random
 	if RANDOMIZE:
@@ -70,12 +105,11 @@ func _process(delta):
 	pass
 
 func _physics_process(delta):
-#	mouse_pos = to_local(get_global_mouse_position())
-#	var tmap_pos = tmap.local_to_map(mouse_pos)
-#	var td = tmap.get_cell_tile_data(0, tmap_pos)
-#	if td:
-#		print(td.get_custom_data('floor'))
-	pass
+	if player.velocity:
+		var tmap_pos = tmap.local_to_map(player.position)
+		var td = tmap.get_cell_tile_data(0, tmap_pos)
+		if td:
+			player.fake_z = td.get_custom_data('floor')
 
 func replace():
 	for rcel in rmap.get_used_cells(0):
@@ -92,6 +126,48 @@ func replace():
 					var tnoise = 2 * (abs(temp_noise.get_noise_2d(pos.x, pos.y)))
 					var mnoise = 2 * (abs(moist_noise.get_noise_2d(pos.x, pos.y)))
 					var ay = HEIGHT_TO_ATLAS[how_high(anoise)]
-					tmap.set_cell(0, pos, 1, Vector2i(1,ay))
-					print(ay)
+					var ax = tile_type[get_tile(anoise, tnoise, mnoise)]
+					tmap.set_cell(0, pos, 1, Vector2i(ax,ay))
 					pass
+
+func get_tile(alt, temp, moist):
+	# sea level
+	if alt < LOWLAND_START:
+		return 'water'
+	# lowlands
+	elif in_range(alt, LOWLAND_START, MIDLAND_START):
+#		return 'beach'
+		return 'sand'
+	# inland
+	elif in_range(alt, MIDLAND_START, HIGHLAND_START):
+		
+		if temp < HIGH_TEMP:
+			# low moisture
+			if moist < MID_HUMID:
+#				return 'desert'
+				return 'sand'
+			# normal moisture
+			elif in_range(moist, MID_HUMID, HIGH_HUMID):
+				return 'drygrass'
+			# high moisture
+			else:
+				return 'wetgrass'
+		else:
+			if moist < HIGH_HUMID:
+				return 'sand'
+			else:
+				return 'wetgrass'
+	# highlands
+	elif in_range(alt, HIGHLAND_START, MOUNTAIN_START):
+#		return 'highlands'
+		if moist < MID_HUMID:
+			return 'dirt'
+		else:
+			return 'wetgrass'
+	# mountains
+	else:
+		if moist > MID_HUMID:
+			return 'snow'
+		else:
+			return 'dirt'
+			
